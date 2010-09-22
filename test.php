1a9e047b90e278bd6bcb950761ffe3177aaed240
Matheus E. Muller <hello@memuller.com>
2010-09-22 07:11:55 -0700

    Confirmado que o bug na busca de livros não ocorre com a biblioteca nuSOAP.

--- test.php
+++ test.php
@@ -1,7 +1,15 @@
 <?php
 require_once('bible_reference.php');
 require_once('includes/haml/HamlParser.class.php');
-$response = BibleReference::client()->listarLivros(   )  ;
-print_r($response);
+require_once('includes/nusoap/nusoap.php');
+//isto funciona (c/ nusoap)
+$client = new nusoap_client( BibleReference::server_url  );
+$response = $client->call( 'listarLivros', array( '', '', 'Ex' ) );
+echo ($response['item']['liv_nome']) ;
+?> <br/> <?php
+// isto não funciona (retorna sempre o gênesis)
+$client = new SoapClient (BibleReference::server_url);
+$response = $client->listarLivros( array( '', '', 'Ex'  )  )   ;
+echo ($response[0]->liv_nome);
 
 ?>
